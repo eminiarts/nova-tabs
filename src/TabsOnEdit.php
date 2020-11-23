67dcc4935c5d35ee59cf80ab790df65e62182ed7
Martin Hansen <martin@myworkout.no>
2020-11-23 21:03:37 +0100

    Fix resource not loaded during update validation
    This commit fixes an issue where the resources properties (like the id)
    were not populated in computed fields during updates when using the
    TabsOnEdit trait.
    
    The change is in a method that is overwritten from Nova 3.15.0, and is
    basically a copy paste from that method.

--- src/TabsOnEdit.php
+++ src/TabsOnEdit.php
@@ -121,8 +121,9 @@ trait TabsOnEdit
      */
     public static function rulesForUpdate(NovaRequest $request, $resource = null)
     {
-        return static::formatRules($request, (new static(static::newModel()))
-                ->parentUpdateFields($request)
+        $resource = $resource ?? self::newResource();
+
+        return static::formatRules($request, $resource->parentUpdateFields($request)
                 ->mapWithKeys(function ($field) use ($request) {
                     return $field->getUpdateRules($request);
                 })->all());
