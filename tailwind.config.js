f3e5fdc3c8353e1bb148add90093c6306351eb81
Marc Filipan <marc@mudsu.com>
2022-04-23 15:21:25 +0200

    fixed bluegray issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const { fontFamily } = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+const {fontFamily} = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
@@ -7,22 +8,14 @@ module.exports = {
   ],
   darkMode: 'class',
   prefix: 'tabs-',
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.slate
+      }
+    }
+  },
   important: true,
   plugins: [],
-  safelist: [
-    'grid-cols-1',
-    'grid-cols-2',
-    'grid-cols-3',
-    'grid-cols-4',
-    'grid-cols-5',
-    'grid-cols-6',
-    'grid-cols-7',
-    'grid-cols-8',
-    'grid-cols-9',
-    'grid-cols-10',
-    'grid-cols-11',
-    'grid-cols-12',
-    'grid-cols-none',
-    'grid'
-  ],
 };
+
