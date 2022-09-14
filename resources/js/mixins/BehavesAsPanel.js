4670dbe83dd3cb87a2ef92803981f9ee21250d1c
Mior Muhammad Zaki <crynobone@gmail.com>
2022-09-15 06:37:07 +0800

    wip

--- resources/js/mixins/BehavesAsPanel.js
+++ resources/js/mixins/BehavesAsPanel.js
@@ -1,6 +1,8 @@
 export default {
   emits: ['actionExecuted'],
 
+  props: ['resourceName', 'resourceId', 'resource', 'panel'],
+
   methods: {
     /**
      * Handle the actionExecuted event and pass it up the chain.
